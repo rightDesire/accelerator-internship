1747602b0071853c13e57a6a8b31326dbd43dcd4
Egor <105357627+rightDesire@users.noreply.github.com>
2024-01-18 20:00:17 +0300

    Добавляет header (без переполнения)

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,28 @@
-// https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
+import {iosVhFix} from './utils/ios-vh-fix';
+import {burger} from './modules/burger';
+import {initAccordions} from './vendor/accordion/init-accordion';
+import {Form} from './vendor/form-validate/form';
+import {ScrollLock} from './utils/scroll-lock';
+import {FocusLock} from './utils/focus-lock';
+// ---------------------------------
 
+window.addEventListener('DOMContentLoaded', () => {
+
+  // Utils
+  // ---------------------------------
+  const scrollLock = new ScrollLock();
+  const focusLock = new FocusLock();
+  iosVhFix();
+  burger(scrollLock, focusLock);
+  initAccordions();
+  // Modules
+
+  // ---------------------------------
+  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
+  // в load следует добавить скрипты, не участвующие в работе первого экрана
+  window.addEventListener('load', () => {
+    const form = new Form();
+    window.form = form;
+    form.init();
+  });
+});
---
+++ source/js/modules/burger.js
@@ -0,0 +1,58 @@
+export const burger = (ScrollLock, FocusLock) => {
+  let prevScrollPos = window.scrollY;
+  const header = document.querySelector('[data-header=""]');
+  const burgerButton = document.querySelector('[data-burger=""]');
+  const body = document.body;
+
+  const closeMenu = () => {
+    ScrollLock.enableScrolling();
+    FocusLock.unlock('[data-header]');
+    document.removeEventListener('keydown', (event) => {
+      if (event.key === 'Escape') {
+        closeMenu();
+      }
+    });
+    header.classList.remove('is-open');
+    body.classList.remove('overlay');
+  };
+
+  const openMenu = () => {
+    ScrollLock.disableScrolling();
+    FocusLock.lock('[data-header]');
+    document.addEventListener('keydown', (event) => {
+      if (event.key === 'Escape') {
+        closeMenu();
+      }
+    });
+    header.classList.add('is-open');
+    body.classList.add('overlay');
+  };
+
+  window.addEventListener('scroll', () => {
+    const currentScrollPos = window.scrollY;
+
+    if (prevScrollPos > currentScrollPos) {
+      header.classList.remove('is-hidden');
+    } else {
+      header.classList.add('is-hidden');
+    }
+
+    prevScrollPos = currentScrollPos;
+  });
+
+  burgerButton.addEventListener('click', () => {
+    if (header.classList.contains('is-open')) {
+      closeMenu();
+    } else {
+      openMenu();
+    }
+  });
+
+  // Close the menu when clicking outside the menu
+  body.addEventListener('click', (event) => {
+    if (!header.contains(event.target) && !burgerButton.contains(event.target)) {
+      header.classList.remove('is-open');
+      body.classList.remove('overlay');
+    }
+  });
+};
